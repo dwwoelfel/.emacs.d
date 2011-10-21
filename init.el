3e34d0824a76240553ca39682ff907290c79e337
Daniel Woelfel <dwwoelfel@gmail.com>
2011-10-20 22:49:45 -0700

    adding init.el and some other .el files

---
+++ init.el
@@ -0,0 +1,71 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(cua-mode t nil (cua-base))
+ '(inhibit-startup-screen t)
+ '(js-indent-level 2)
+ '(menu-bar-mode t)
+ '(scroll-bar-mode (quote right))
+ '(show-paren-mode t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
+
+(let ((default-directory "~/.emacs.d/site-lisp/"))
+  (setq load-path
+	(append
+	 (let ((load-path (copy-sequence load-path))) ;; Shadow
+	   (append 
+	    (copy-sequence (normal-top-level-add-to-load-path '(".")))
+	    (normal-top-level-add-subdirs-to-load-path)))
+	 load-path)))
+
+(require 'haml-mode)
+(add-hook 'haml-mode-hook
+	  '(lambda ()
+	     (setq indent-tabs-mode t)
+	     (define-key haml-mode-map "\C-m" 'newline-and-indent)))
+
+(require 'sass-mode)
+
+(load "auctex.el" nil t t)
+(load "preview-latex.el" nil t t)
+
+(show-paren-mode 1)
+(tool-bar-mode -1)
+(setq frame-title-format "%b")
+(setq default-tab-width 2)
+;;(setq tab-stop-list '(2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30))
+
+
+(setq org-cycle-include-plain-lists nil)
+
+;(require 'color-theme)
+;(eval-after-load "color-theme"
+;  '(progn
+;     (color-theme-initialize)
+;     (color-theme-hober)))
+
+; (transient-mark-mode 1)  ; Now on by default: makes the region act quite like the text "highlight" in many apps.
+   ; (setq shift-select-mode t) ; Now on by default: allows shifted cursor-keys to control the region.
+   (setq mouse-drag-copy-region nil)  ; stops selection with a mouse being immediately injected to the kill ring
+   (setq x-select-enable-primary nil)  ; stops killing/yanking interacting with primary X11 selection
+   (setq x-select-enable-clipboard t)  ; makes killing/yanking interact with clipboard X11 selection
+   ;; these will probably be already set to these values, leave them that way if so!
+   ; (setf interprogram-cut-function 'x-select-text)
+   ; (setf interprogram-paste-function 'x-cut-buffer-or-selection-value)
+   ; You need an emacs with bug #902 fixed for this to work properly. It has now been fixed in CVS HEAD.
+   ; it makes "highlight/middlebutton" style (X11 primary selection based) copy-paste work as expected
+   ; if you're used to other modern apps (that is to say, the mere act of highlighting doesn't
+   ; overwrite the clipboard or alter the kill ring, but you can paste in merely highlighted
+   ; text with the mouse if you want to)
+   (setq select-active-regions t) ;  active region sets primary X11 selection
+   (global-set-key [mouse-2] 'mouse-yank-primary)  ; make mouse middle-click only paste from primary X11 selection, not clipboard and kill ring.
+   ;; with this, doing an M-y will also affect the X11 clipboard, making emacs act as a sort of clipboard history, at
+   ;; least of text you've pasted into it in the first place.
+   ; (setq yank-pop-change-selection t)  ; makes rotating the kill ring change the X11 clipboard.
