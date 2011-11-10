63d007a3f30e835a27f626f3a602b8f10be6e74d
Daniel Woelfel <dwwoelfel@gmail.com>
2011-11-10 15:18:11 -0800

    adds smooth scrolling

--- init.el
+++ init.el
@@ -14,7 +14,7 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 79 :width normal :foundry "microsoft" :family "Consolas")))))
 
 (let ((default-directory "~/.emacs.d/site-lisp/"))
   (setq load-path
@@ -24,6 +24,16 @@
 	    (copy-sequence (normal-top-level-add-to-load-path '(".")))
 	    (normal-top-level-add-subdirs-to-load-path)))
 	 load-path)))
+
+(require 'smooth-scrolling)
+
+(setq mouse-wheel-scroll-amount '(5 ((shift) . 5))) ;; five lines at a time
+
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+    
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+    
+(setq scroll-step 1) ;; keyboard scroll one line at a time
 
 (require 'haml-mode)
 (add-hook 'haml-mode-hook
