f510dcf45bfed95d1e75848067de3fa55df48288
daniel <dwwoelfel@gmail.com>
2012-01-18 09:02:20 -0800

    adds slime for clojure, zenburn, magit, and some keyboard shortcuts

---
+++ elpa/zenburn-1.8/zenburn-autoloads.el
@@ -0,0 +1,28 @@
+;;; zenburn-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (color-theme-zenburn) "zenburn" "zenburn.el" (20244
+;;;;;;  47890))
+;;; Generated autoloads from zenburn.el
+
+(autoload 'color-theme-zenburn "zenburn" "\
+Just some alien fruit salad to keep you in the zone.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("zenburn-pkg.el") (20244 47890 609171))
+
+;;;***
+
+(provide 'zenburn-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; zenburn-autoloads.el ends here
--- init.el
+++ init.el
@@ -8,17 +8,18 @@
  '(ido-enable-flex-matching t)
  '(inhibit-startup-screen t)
  '(js-indent-level 2)
- '(menu-bar-mode t)
  '(scroll-bar-mode (quote right))
  '(show-paren-mode t)
- '(size-indication-mode t))
+ '(size-indication-mode t)
+ '(tool-bar-mode nil))
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 79 :width normal :foundry "microsoft" :family "Consolas")))))
+ '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "microsoft" :family "Consolas")))))
 
+;; load path
 (let ((default-directory "~/.emacs.d/site-lisp/"))
   (setq load-path
 	(append
@@ -41,6 +42,12 @@
 (setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
     
 (setq scroll-step 1) ;; keyboard scroll one line at a time
+
+;; open links in google chrome
+(setq browse-url-browser-function 'browse-url-generic
+      browse-url-generic-program "google-chrome")
+
+(require 'magit)
 
 ;; Rails stuff
 
@@ -107,6 +114,10 @@
 	(interactive)
 	(ansi-color-apply-on-region (point-min) (point-max))
 )
+
+;; some keyboard shortcuts
+(global-set-key (kbd "C-<f10>") 'revert-buffer)
+(global-set-key (kbd "C-<f9>") 'magit-status)
 
 ;; this would be better if I could get the point whenever the buffer
 ;; reverted. Then I could pass that point into colorize-ansi for an automatic
