f481fd84a22e0cd2a648f5ee1ddf8cb61365b99d
Daniel Woelfel <dwwoelfel@gmail.com>
2012-10-23 06:19:44 -0700

    problems with relative paths

--- init.el
+++ init.el
@@ -8,18 +8,18 @@
  '(css-indent-offset 2)
  '(cua-mode t nil (cua-base))
  '(font-use-system-font t)
- '(icicle-download-dir "~/.emacs.d/site-lisp/icicles")
+ '(icicle-download-dir "/home/daniel/.emacs.d/site-lisp/icicles")
  '(ido-enable-flex-matching t)
  '(inhibit-startup-screen t)
  '(js-indent-level 2)
  '(less-css-lessc-options (quote ("-x")))
- '(org-agenda-files (quote ("~/Dropbox/cakehealth/cake_health.org")))
+ '(org-agenda-files (quote ("/home/daniel/Dropbox/cakehealth/cake_health.org")))
  '(org-modules (quote (org-bbdb org-bibtex org-docview org-gnus org-info org-jsinfo org-irc org-mew org-mhe org-rmail org-vm org-wl org-w3m org-drill)))
  '(safe-local-variable-values (quote ((less-css-output-directory . "../css") (less-css-compile-at-save . t) (ruby-compilation-executable . "ruby") (ruby-compilation-executable . "ruby1.8") (ruby-compilation-executable . "ruby1.9") (ruby-compilation-executable . "rbx") (ruby-compilation-executable . "jruby"))))
  '(scroll-bar-mode (quote right))
  '(show-paren-mode t)
  '(size-indication-mode t)
- '(temporary-file-directory "~/.saves")
+ '(temporary-file-directory "/home/daniel/.saves")
  '(tool-bar-mode nil)
  '(use-file-dialog nil))
 (custom-set-faces
@@ -32,7 +32,7 @@
  '(flymake-warnline ((((class color)) (:background "#EEEE00")))))
 
 ;; load path
-(let ((default-directory "~/.emacs.d/site-lisp"))
+(let ((default-directory "/home/daniel/.emacs.d/site-lisp"))
   (setq load-path
 	(append
 	 (let ((load-path (copy-sequence load-path))) ;; Shadow
@@ -182,7 +182,7 @@
      (when (not window-system)
        (set-face-background 'magit-item-highlight "black"))))
 
-(load "~/.emacs.d/site-lisp/haskell/haskell-site-file")
+(load "/usr/home/daniel/.emacs.d/site-lisp/haskell/haskell-site-file")
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 
@@ -281,7 +281,7 @@
 ;; Lisp stuff
 
 ;(setq inferior-lisp-program "/usr/local/bin/sbcl") ; your Lisp system
-;(add-to-list 'load-path "~/.emacs.d/site-lisp/slime/")  ; your SLIME directory
+;(add-to-list 'load-path "/home/daniel/.emacs.d/site-lisp/slime/")  ; your SLIME directory
 ;(require 'slime)
 ;(slime-setup)
 
@@ -312,7 +312,7 @@
 
 (setq auto-save-file-name-transforms
 			'(("\\`/[^/]*:\\([^/]*/\\)*\\([^/]*\\)\\'" "/tmp/\\2" t)
-				("\\`/?\\([^/]*/\\)*\\([^/]*\\)\\'" "~/.saves/\\2" t)))
+				("\\`/?\\([^/]*/\\)*\\([^/]*\\)\\'" "/home/daniel/.saves/\\2" t)))
 
 ;; org-mode
 (setq org-cycle-include-plain-lists nil)
@@ -585,7 +585,7 @@
 ;;; packages in your .emacs.
 ;(when
 ;		(load
-;		 (expand-file-name "~/.emacs.d/elpa/package.el"))
+;		 (expand-file-name "/home/daniel/.emacs.d/elpa/package.el"))
 ;	(package-initialize))
 
 ;; wrapping
